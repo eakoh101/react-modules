5475cee9354e976b2460fb41faedbcc10df37f70
Eakkarach <eakoh101@gmail.com>
2026-03-25 23:02:32 +0700

    test:react-module

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from './App.jsx'
 import './index.css'
+// 1. เพิ่มบรรทัด Import นี้ (Path ตามที่ snippet สร้างให้)
+import { Provider } from "./components/ui/provider" 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* 2. นำ Provider มาครอบ App แบบนี้ */}
+    <Provider>
+      <App />
+    </Provider>
   </React.StrictMode>,
 )
+
